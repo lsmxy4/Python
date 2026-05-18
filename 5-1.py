5279218455e24b35143727346a5d8335a732903b
unknown <lsmxy4@gmail.com>
2026-05-18 18:18:55 +0900

    5weak

---
+++ 5-1.py
@@ -0,0 +1,5 @@
+import asyncio
+async def main():
+    print("hello")
+
+asyncio.run(main())
---
+++ 5-2.py
@@ -0,0 +1,15 @@
+import asyncio
+async def job(name,delay):
+    await asyncio.sleep(delay)
+
+    return name
+
+async def main():
+    results = await asyncio.gather(
+        job("A",2),
+        job("B",1)
+    )
+    print(results)
+
+if __name__ == "__main__":
+    asyncio.run(main())
